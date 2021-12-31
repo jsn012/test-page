54349c77b47f121178a15ba87e78d1771c20a64d
maynut <libes1234@naver.com>
2021-12-31 22:38:25 +0900

    upload img file

--- src/components/GameInfo.js
+++ src/components/GameInfo.js
@@ -8,36 +8,33 @@ function GameInfo() {
   const id = useParams();
   const navigate = useNavigate();
 
-  const btnClick = () => {
-    navigate('/test-page/game/main')
-  };
-
-  const gameIcon = {
-    backgroundImage: `url(${process.env.PUBLIC_URL + gameListData[id.gameId].icon})`
-  }
+  const gameIcon = { backgroundImage: `url(${process.env.PUBLIC_URL + gameListData[id.gameId].icon})` };
+  const mainImg = { backgroundImage: `url(${process.env.PUBLIC_URL + gameListData[id.gameId].img[0]}`};
 
   return(
     <div className="game-info">
-      <div className="game-video">
-        {gameListData[id.gameId].video}
-      </div>
-      <div className="game-icon" style={gameIcon}>
-      </div>
-      <div className="game-title">
-        <div className="game-title_kr">
-          {gameListData[id.gameId].titleKR}
+      <div className="game-img_main" style={mainImg}></div>
+      <div className="game-info__inner">
+        <div className="game-icon" style={gameIcon}>
         </div>
-        <div className="game-title_cn">
-          {gameListData[id.gameId].titleCN}
+        <div className="game-title">
+          <div className="game-title_kr">
+            {gameListData[id.gameId].titleKR}
+          </div>
+          <div className="game-title_cn">
+            {gameListData[id.gameId].titleCN}
+          </div>
+          <div className="game-title_en">
+            {gameListData[id.gameId].titleEN}
+          </div>
         </div>
-        <div className="game-title_en">
-          {gameListData[id.gameId].titleEN}
+        <div className="game-video">
+          {gameListData[id.gameId].video}
+        </div>
+        <div className="game-info">
+          {gameListData[id.gameId].info}
         </div>
       </div>
-      <div className="game-info">
-        {gameListData[id.gameId].info}
-      </div>
-      <button type="button" onClick={btnClick}>Back</button>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,31 @@ import '../Header.css'
 function Header() {
   const navigate = useNavigate();
   const titleClick = () => {
-    navigate('/test-page')
+    navigate(-1);
   };
 
   return(
     <header>
-      <div className="title" onClick={titleClick}>
-        타이틀
+      <div className="header_inner">
+        <div className="header_inner-left">
+          <div className="header_btn back-btn" onClick={titleClick}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-arrow-left-short" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z" strokeWidth="3" />
+            </svg>
+          </div>
+        </div>
+        <div className="header_inner-right">
+          <div className="header_btn option-btn">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-three-dots-vertical" viewBox="0 0 16 16">
+              <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+            </svg>
+          </div>
+          <div className="header_btn search-btn">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" strokeWidth="3" />
+            </svg>
+          </div>
+        </div>
       </div>
     </header>
   );
